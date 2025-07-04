a6a92780350bc91b8572564c34ffddb0b5314f3a
Tanguy VANSNICK <191451@umons.ac.be>
2025-07-04 21:49:05 +0200

    fast api hello world

--- tee/main.py
+++ tee/main.py
@@ -1,1 +1,11 @@
-print("hello wrold")
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.post("/")
+async def post_root():
+    return {"message": "Hello World", "method": "POST"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
